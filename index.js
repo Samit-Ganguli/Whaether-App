096b0d9d2c84c0236c5ce2dba175739249ae76db
samitGanguli2 <samit91@vt.edu>
2020-03-21 18:56:09 -0400

    Made some changes

--- index.js
+++ index.js
@@ -2,53 +2,37 @@ var express = require("express");
 var app = express();
 var Request = require("request");
 
- var returnJson;
  var port = process.env.PORT || 8080;
-app.listen(port, function () {
+ app.listen(port, function () {
      console.log("Running RestHub on port " + port);
 });
- var urlString1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
- var urlString2 = ',us&appid=16de6e612ac71e50cee69605c800b11e';
-	app.get('/locations/:zipcode/:scale?', function(req, res) {
-	var id = req.params.zipcode;
-	var scale = req.query.scale;	
-	var urlString3 = urlString1+id+urlString2;
-	Request.get(urlString3, (error, response, body) => {
-	if(error) {
-		return res.send("Please enter a valid zip code");
-		process.exit(1);
-    }else if(response){
-		console.log(body);
-		var prettyJson = JSON.parse(body);
-		if(prettyJson.cod == 404){
-			return res.send("Please enter a valid zip code");
-		}
-		var temp = prettyJson.main.temp;
-		temp = (temp-273.15)*(9/5) + 32;
-		if(req.query.scale){
-			if((scale == "celcius")||(scale == "Celcius")){
-			temp = (temp - 32)*(5/9)
-			returnJson = {"temperature":temp, "scale":scale};
-			console.log(temp);
-		};
-		if((scale == "kelvin")||(scale == "Kelvin")){
-			temp = (temp - 32)*(5/9) + 273.15;
-			returnJson = {"temperature":temp, "scale":scale};
-			console.log(temp);
-		};
-		if(scale == "fahrenheit"){
-			returnJson = {"temperature":temp, "scale":scale};
-			console.log(temp);
-		};
-		if((scale != "fahrenheit")&&(scale != "kelvin")&&(scale != "celcius")&&(scale != "Fahrenheit")&&(scale != "Kelvin")&&(scale != "Celcius")){
-		// else{	
-			returnJson = {"temperature":temp, "scale":scale, "Message":"Currently showing temperature in Fahrenheit, kindly enter a valid scale in future. This app only supports Celcius, Kelvin and Fahrenheit."};
-		}
-		}else{
-			returnJson = {"temperature":temp, "scale":"Fahrenheit"};
-		}		
-		res.send(returnJson);
-		return returnJson;
-		};	
-		});
-	});
+
+	app.get('/locations/:zipcode', (req, res) => {
+	const zip_code = req.params.zipcode;
+	let query_scale = req.query.scale === undefined || req.query.scale === 'Fahrenheit' ? 'Fahrenheit' : 'Celsius'
+	let scale = query_scale === 'Fahrenheit' ? 'imperial' : 'metric'; 
+	let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	let weather_req = new XMLHttpRequest();
+	let url = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip_code+',us&units='+scale+'&appid=16de6e612ac71e50cee69605c800b11e';
+	let temperature = 0;
+	let ifError = true;
+	weather_req.open('GET',url,false);
+	weather_req.onreadystatechange = function() {
+				if (weather_req.readyState == 4 && weather_req.status == 200) {
+					temperature = JSON.parse(weather_req.responseText).main.temp
+					ifError = false
+				}
+			}
+	weather_req.send();
+	let answer = {
+		'temperature':temperature,
+		'scale': query_scale
+	}
+	if(ifError){
+		res.status(400);
+		res.send("Invalid Request");
+	}else{
+		res.status(200);
+		res.send(answer);
+	}
+});
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 var assert = require('assert');
 
 it('Valid Zip Code and Scale Test', function(done) {
-    request('http://localhost:8080/locations/24060?scale=celcius' , function(error, response, body) {
+    request('http://localhost:8080/locations/24060?scale=celsius' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
@@ -14,22 +14,16 @@ it('Valid Zip Code and Scale Test', function(done) {
         done();
     });
 });
-it('Valid Zip Code and Invalid Scale Test', function(done) {
-    request('http://localhost:8080/locations/24060?scale=celciusggg' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-		// assert.equal(body,'{"temperature":34.97000000000006,"scale":"celciusggg","Message":"Currently showing temperature in Fahrenheit, kindly enter a valid scale in future. This app only supports Celcius, Kelvin and Fahrenheit."}');
-        done();
-    });
-});
+
 it('Invalid Zip Code and valid Scale Test', function(done) {
-    request('http://localhost:8080/locations/2406000?scale=celcius' , function(error, response, body) {
-        expect(body).to.equal('Please enter a valid zip code');
+    request('http://localhost:8080/locations/2406000?scale=celsius' , function(error, response, body) {
+        expect(body).to.equal('Invalid Request');
         done();
     });
 });
 it('Invalid Zip Code and Invalid Scale Test', function(done) {
-    request('http://localhost:8080/locations/2406000?scale=celciusssss' , function(error, response, body) {
-        expect(body).to.equal('Please enter a valid zip code');
+    request('http://localhost:8080/locations/24060?scale=Fahrenheit' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
         done();
     });
 });
